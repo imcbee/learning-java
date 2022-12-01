54886db47cd48916deba39b85d8ef1e224afbf8f
Ian <imcbee@terpmail.umd.edu>
2022-12-01 11:01:57 -0500

    updated reverseString program

--- ReverseString.java
+++ ReverseString.java
@@ -1,23 +1,30 @@
-//! Simplae class to be able to reverse a string from a scanner input
+//! Simple class to be able to reverse a string from a scanner input
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class ReverseString {
     public static void main(String[] args) {
-        // create a userinput with scanner
+        // create a userInput with scanner
         Scanner scanner = new Scanner(System.in);
 
         // create string
         System.out.print("Type in a word: ");
         String str = scanner.nextLine();
 
+        //convert string into an array
+        char str1[] = str.toCharArray();
+
+        //new var for new reversed string
+        ArrayList<Character> newStr = new ArrayList<Character>();
+
 
         // loop through string backwards
-        for(int i=str.length()-1; i>=0; i--) {
-            System.out.print(str.charAt(i));
+        for(int i=str1.length-1; i>=0; i--) {
+            newStr.add(str1[i]);
         }
 
+        System.out.println(newStr);
         scanner.close();
     }
 }
